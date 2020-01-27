b30cb867b3982a4e457dd90f52b9c965b2e4fe3b
Claudio Antonius <antonius.claudio@gmail.com>
2020-01-27 22:15:51 +0700

    Week 2 Exercise 4

---
+++ Week2/Exercises4.js
@@ -0,0 +1,44 @@
+// Switch-Case - Claudio Antonius
+let tanggal = 1;
+let bulan = 12;
+let tahun = 1945;
+
+switch (bulan) {
+    case 1:
+        month = 'Januari';
+        break;
+    case 2:
+        month = 'Februari';
+        break;
+    case 3:
+        month = 'Maret';
+        break;
+    case 4:
+        month = 'April';
+        break;
+    case 5:
+        month = 'Mei';
+        break;
+    case 6:
+        month = 'Juni';
+        break;
+    case 7:
+        month = 'Juli';
+        break;
+    case 8:
+        month = 'Agustus';
+        break;
+    case 9:
+        month = 'September';
+        break;
+    case 10:
+        month = 'Oktober';
+        break;
+    case 11:
+        month = 'November';
+        break;
+    case 12:
+        month = 'Desember';
+        break;
+}
+console.log(tanggal + ' ' + month + ' ' + tahun);
