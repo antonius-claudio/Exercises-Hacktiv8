1576541d06fe5d798eeb51a5996beacd8c621958
Claudio Antonius <antonius.claudio@gmail.com>
2020-01-29 22:12:19 +0700

    Palindrome Angka

---
+++ Week3/Palindrome_Angka.js
@@ -0,0 +1,37 @@
+// Claudio Antonius
+
+// manual
+function angkaPalindrome(angka){
+    // variable untuk check sudah palindrome atau belum
+    let condition = false;
+    
+    // looping mulai dari angka 1 berikutnya bertambah 1 sampai kondisi true atau polindrome
+    for (let i = (angka+1); condition==false ; i++) {
+        // mengubah angkaNext menjadi string agar bisa dibalik
+        let stringAngkaNext_i = '';
+        stringAngkaNext_i = `${i}`;
+        // variable untuk reverse
+        let reverseNext_i = '';
+
+        // looping untuk reverse kata
+        for (let j = stringAngkaNext_i.length; j > 0; j--) {
+            reverseNext_i += stringAngkaNext_i[j-1];
+        }
+        // console.log(reverseNext_i);
+        // console.log(stringAngkaNext_i);
+
+        // untuk cek palindrome atau belum
+        if (stringAngkaNext_i === reverseNext_i) {
+            // console.log(stringAngkaNext_i);
+            condition = true;
+            return stringAngkaNext_i
+        }
+        
+    }
+}
+
+console.log(angkaPalindrome(8)); // 9
+console.log(angkaPalindrome(10)); // 11
+console.log(angkaPalindrome(117)); // 121
+console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(1000)); // 1001
