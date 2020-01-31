bc6c77d9cd5edd6d559096dbbf85b063c0ac4b5b
Claudio Antonius <antonius.claudio@gmail.com>
2020-01-31 21:57:38 +0700

    Faktor Persekutuan Terbesar

--- Week4/Faktor_Persekutuan_Terbesar.js
+++ Week4/Faktor_Persekutuan_Terbesar.js
@@ -1,13 +1,41 @@
 // Claudio Antonius
 
 function fpb(angka1, angka2) {
-    let faktor1 = [];
-    let faktor2 = [];
-    for (let i = 1; i < angka1; i++) {
-        if (angka1%i===0)
-            faktor1.push(i);
+    // variable simpan faktor tiap angka
+    let faktor1 = faktor(angka1);
+    let faktor2 = faktor(angka2);
+
+    // karena sama, bisa dijadikan function lain
+    // // mencari faktor variable angka1
+    // for (let i = 1; i <= angka1; i++) {
+    //     if (angka1%i===0)
+    //         faktor1.push(i);
+    // }
+    // // mencari faktor variable angka 2
+    // for (let j = 1; j <= angka2; j++) {
+    //     if (angka2%j===0)
+    //         faktor2.push(j);      
+    // }
+
+    // mencari fpb
+    let fpb = 0;
+    for (let k = 0; k < faktor1.length; k++) {
+        for (let l = 0; l < faktor2.length; l++) {
+            if(faktor1[k] === faktor2[l]){
+                fpb = faktor1[k];
+            }            
+        }        
     }
-    return faktor1;
+    return fpb;
+}
+
+function faktor(number) {
+    let temp = [];
+    for (let i = 1; i <= number; i++) {
+        if (number%i===0)
+            temp.push(i);
+    }
+    return temp;
 }
 
 // TEST CASES
